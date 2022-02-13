ccc5feae0d1a3bd15c45326b51507302099dea62
tanveer <mdtanviralamht731@gmail.com>
2022-02-13 23:54:57 +0600

    add the mutiple color

--- js/app.js
+++ js/app.js
@@ -2,16 +2,22 @@
 const button = document.getElementById('random-btn');
 // console.log(button);
 button.addEventListener('click',function(){
+    // color calling function
     const myColor1 = coloGenerate1()
     const myColor2 = coloGenerate3()
     const myColor3 = coloGenerate3()
-    console.log(myColor1);
-    console.log(myColor2);
-    console.log(myColor3);
-    const colorDisplay = document.getElementById('color-display');
-    colorDisplay.style.backgroundColor = `rgb(${myColor1},${myColor2},${myColor3})`;
-    document.getElementById('color-text').innerText = `rgb(${myColor1},${myColor2},${myColor3});`;
-
+    // add multiple box 
+    const parent = document.getElementById('parent');
+    const otherDiv = document.createElement('div')
+    otherDiv.classList.add('other');
+    const child = `
+    <div id="color-display" style="background-color: rgb(${myColor1},${myColor2},${myColor3})" class="color">
+        <h3 id="color-text">rgb(${myColor1},${myColor2},${myColor3});</h3>
+    </div>
+    `;
+    // set value
+    otherDiv.innerHTML = child;
+    parent.appendChild(otherDiv);
 });
 function coloGenerate1(){
     const color = Math.round(Math.random()*256);
