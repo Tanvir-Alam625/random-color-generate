bfebd091d937e2dd0d7cd0939532ac590241ef72
tanveer <mdtanviralamht731@gmail.com>
2022-02-19 22:43:46 +0600

    one problem solved

--- js/app.js
+++ js/app.js
@@ -11,14 +11,41 @@ button.addEventListener('click',function(){
     const otherDiv = document.createElement('div')
     otherDiv.classList.add('other');
     const child = `
-    <div id="color-display" style="background-color: rgb(${myColor1},${myColor2},${myColor3})" class="color">
-        <h3 id="color-text">rgb(${myColor1},${myColor2},${myColor3});</h3>
+    <div id="color-display" style="background-color: rgb(${myColor1},${myColor2},${myColor3}); cursor: pointer" class="color">
+        <h3 id="color-text" disabled >rgb(${myColor1},${myColor2},${myColor3});</h3>
     </div>
+        <span style="color: rgb(${myColor1},${myColor2},${myColor3}); display:none">Copied</span>
     `;
     // set value
     otherDiv.innerHTML = child;
     parent.appendChild(otherDiv);
+    /////////////////////////////////////
+    const colorItems = document.getElementsByClassName('color');
+    for(const item of colorItems){
+        item.addEventListener('click',function(event){
+            const events = event.target.childNodes[1].innerText;
+            console.log(events);
+            event.stopImmediatePropagation();
+            getColorValue(events)
+
+        });
+    }
 });
+
+function getColorValue(text){
+    let input = document.body.appendChild(document.createElement('input'));
+    input.value = text;
+    input.focus();
+    input.select();
+    document.execCommand('copy');
+    input.parentNode.removeChild(input);
+
+}
+
+
+
+
+
 function coloGenerate1(){
     const color = Math.round(Math.random()*256);
     return color
