4a02ba4180c53c3752b5bff1c8b53acf28cf2dc5
tanveer <mdtanviralamht731@gmail.com>
2022-03-13 21:00:02 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -7,28 +7,22 @@ button.addEventListener('click',()=>{
     const blue = Math.round(Math.random()*256);
     // add multiple box 
     const parent = document.getElementById('parent');
-    const otherDiv = document.createElement('div')
+    const otherDiv = document.createElement('div');
     otherDiv.classList.add('other');
+    const rgb = `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
     const child = `
-    <div id="color-display" style="background-color: rgb(${red},${green},${blue});" class="color">
-        <h3 id="color-text" disabled >rgb(${red},${green},${blue});</h3>
-        <img class="copy-icon" src="./images/copy-solid.svg" width="20" alt="icon">
+    <div class="color-container">
+    <span class="message show-message" style="color:${rgb};">copied</span>
+        <div id="color-display" style="background-color: ${rgb};" class="color">
+            <h3 id="color-text" disabled >${rgb}</h3>
+            <img title="copy to clipboard" class="copy-icon" src="./images/copy-solid.svg" width="20" alt="icon">
+        </div> 
     </div>
-        <span style="color: rgb(${red},${green},${blue}); display:none">Copied</span>
     `;
     // set value
     otherDiv.innerHTML = child;
     parent.appendChild(otherDiv);
     getColorValue();
-    /////////////////////////////////////
-    // const colorItems = document.getElementsByClassName('copy-icon');
-    // for(const item of colorItems){
-    //     item.addEventListener('click',function(event){
-    //         const events = event.target.parentNode.childNodes[1].innerText;
-    //         event.stopImmediatePropagation();
-    //         getColorValue(events);
-    //     });
-    // }
 });
 // get color value function 
 const getColorValue = ()=>{
@@ -36,8 +30,14 @@ const getColorValue = ()=>{
     for(const item of colorItems){
         item.addEventListener('click',function(event){
             const events = event.target.parentNode.childNodes[1].innerText;
+            const messages = event.target.parentNode.parentNode.childNodes[1];
             event.stopImmediatePropagation();
             copyToClipboard(events);
+            showMessage(messages);
+            setTimeout(()=>{
+                messages.classList.remove('showMsg');
+                messages.classList.add('removeMsg');
+            },3000)
         });
     }
 }
@@ -49,5 +49,10 @@ const copyToClipboard=(text)=>{
     input.select();
     navigator.clipboard.writeText(input.value)
     input.parentNode.removeChild(input);
+}
+// show message function
+const showMessage = (msg)=>{
+    msg.classList.add('showMsg');
+    msg.classList.remove('removeMsg');
 
 }
